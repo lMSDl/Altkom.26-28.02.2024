f326683ecd462128f6a4ff09a388cd7d029c4ad2
Paweł Łęczycki <pawel@msd.info.pl>
2024-02-26 13:37:36 +0100

    LSP

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Altkom._26_28._02._2024.SOLID.L;
+
+
+int a = 3;
+int b = 5;
+
+Rectangle rectangle = CreateRectangle(a, b);
+
+Console.WriteLine($"{a} * {b} = {rectangle.Area}");
+
+
+
+
+
+Rectangle CreateRectangle(int a, int b)
+{
+    return new Square() { A = a, B = b };
+}
+
+
---
+++ SOLID/III_LSP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._26_28._02._2024.SOLID.L
+{
+
+    abstract class Vehicle
+    {
+        public string Name { get; set; }
+        //public abstract void Fly();
+        public abstract void Move();
+    }
+
+    class Airplane : Vehicle
+    {
+        public void Fly()
+        {
+            Console.WriteLine("I am flying!");
+        }
+
+        public override void Move()
+        {
+            Fly();
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("I am drivig!");
+        }
+
+        public override void Move()
+        {
+            Drive();
+        }
+    }
+}
