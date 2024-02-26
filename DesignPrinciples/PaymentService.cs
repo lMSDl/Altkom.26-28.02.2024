f8c37b4335f7f751364e55e95ed4f288700faea0
Paweł Łęczycki <pawel@msd.info.pl>
2024-02-26 11:33:13 +0100

    DEMETER

--- DesignPrinciples/PaymentService.cs
+++ DesignPrinciples/PaymentService.cs
@@ -8,15 +8,15 @@ namespace Altkom._26_28._02._2024.DesignPrinciples
 {
     public class PaymentService
     {
-        public bool Charge(Customer customer, float amount)
+
+        public bool Charge(PaymentAccount account, float amount)
         {
-            var account = customer?.PaymentAccount;
             return account?.Charge(amount) ?? false;
         }
 
-        public void Fund(Customer customer, float amount)
+        public void Fund(PaymentAccount account, float amount)
         {
-            customer?.PaymentAccount.Fund(amount);
+            account?.Fund(amount);
         }
     }
 }
