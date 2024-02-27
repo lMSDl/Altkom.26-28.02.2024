2167cf75d0fa6f53edb95236137e5c9f49136e84
Paweł Łęczycki <pawel@msd.info.pl>
2024-02-27 15:53:50 +0100

    PrivateClassData II

--- DesignPatterns/StructuralPatterns/PrivateClassData/ClassData.cs
+++ DesignPatterns/StructuralPatterns/PrivateClassData/ClassData.cs
@@ -2,19 +2,25 @@
 {
     internal class ClassData
     {
-        private readonly int _intValue;
-        private string StringValue { get; }
+        //private readonly int _intValue;
+        //private string StringValue { get; }
 
+
+        private PrivateClassData _data;
         public ClassData(int intValue, string stringValue)
         {
-            _intValue = intValue;
-            StringValue = stringValue;
+            //_intValue = intValue;
+            //StringValue = stringValue;
+            _data = new PrivateClassData(intValue, stringValue);
         }
 
         public void DoSth()
         {
             //_intValue = 0;
             //_stringValue = "";
+
+            _data.GetIntValue();
+            _data.GetStringValue();
         }
     }
 }
---
+++ DesignPatterns/StructuralPatterns/PrivateClassData/PrivateClassData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._26_28._02._2024.DesignPatterns.StructuralPatterns.PrivateClassData
+{
+    internal class PrivateClassData
+    {
+        private int _intValue;
+        private string _stringValue;
+
+        public PrivateClassData(int intValue, string stringValue)
+        {
+            _intValue = intValue;
+            _stringValue = stringValue;
+        }
+
+        public int GetIntValue()
+        {
+            return _intValue;
+        }
+
+        public string GetStringValue() { return _stringValue; }
+    }
+}
