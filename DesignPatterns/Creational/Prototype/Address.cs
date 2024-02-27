c9059d9cc3f41bbda6beebaf61c7bfeb488b77b2
Paweł Łęczycki <pawel@msd.info.pl>
2024-02-27 09:08:35 +0100

    Prototype I

---
+++ DesignPatterns/Creational/Prototype/Address.cs
@@ -0,0 +1,8 @@
+﻿namespace Altkom._26_28._02._2024.DesignPatterns.Creational.Prototype
+{
+    public class Address
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+}
---
+++ DesignPatterns/Creational/Prototype/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Altkom._26_28._02._2024.DesignPatterns.Creational.Prototype
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Address Address { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
