0647ea3cc388d2e518f5cf936fd39c6ec69e5f46
Paweł Łęczycki <pawel@msd.info.pl>
2024-02-27 14:41:13 +0100

    Flyweight I

---
+++ DesignPatterns/StructuralPatterns/Flyweight/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._26_28._02._2024.DesignPatterns.StructuralPatterns.Flyweight
+{
+    internal class Car
+    {
+        public string Owner { get; set; }
+        public string Number { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ DesignPatterns/StructuralPatterns/Flyweight/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._26_28._02._2024.DesignPatterns.StructuralPatterns.Flyweight
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var car1 = new Car
+            {
+                Number = "CL234IR",
+                Owner = "James Doe",
+                Manufacturer = "BMW",
+                Model = "M5",
+                Color = "Red"
+            };
+
+            var car2 = new Car
+            {
+                Number = "CL4127A",
+                Owner = "Tom Downton",
+                Manufacturer = "BMW",
+                Model = "M6",
+                Color = "Red"
+            };
+        }
+    }
+}
