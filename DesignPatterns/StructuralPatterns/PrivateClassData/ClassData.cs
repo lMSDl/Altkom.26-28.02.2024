cb36979715890f925f609216561bf0c5f71a1bfe
Paweł Łęczycki <pawel@msd.info.pl>
2024-02-27 15:49:11 +0100

    PrivateClassData I

---
+++ DesignPatterns/StructuralPatterns/PrivateClassData/ClassData.cs
@@ -0,0 +1,20 @@
+﻿namespace Altkom._26_28._02._2024.DesignPatterns.StructuralPatterns.PrivateClassData
+{
+    internal class ClassData
+    {
+        private readonly int _intValue;
+        private string StringValue { get; }
+
+        public ClassData(int intValue, string stringValue)
+        {
+            _intValue = intValue;
+            StringValue = stringValue;
+        }
+
+        public void DoSth()
+        {
+            //_intValue = 0;
+            //_stringValue = "";
+        }
+    }
+}
