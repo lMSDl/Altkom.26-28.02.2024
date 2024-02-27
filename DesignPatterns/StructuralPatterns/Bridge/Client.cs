e3bd8d943c5a483bbd56ba3dbeab0325ce49d85a
Paweł Łęczycki <pawel@msd.info.pl>
2024-02-27 11:50:42 +0100

    Bridge I

---
+++ DesignPatterns/StructuralPatterns/Bridge/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._26_28._02._2024.DesignPatterns.StructuralPatterns.Bridge
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            Shape shape = new Square();
+            Console.WriteLine(shape);
+            Color color = new Red();
+            Console.WriteLine(color);
+        }
+    }
+}
---
+++ DesignPatterns/StructuralPatterns/Bridge/Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._26_28._02._2024.DesignPatterns.StructuralPatterns.Bridge
+{
+    internal abstract class Shape
+    {
+        public override string ToString()
+        {
+            return GetType().Name;
+        }
+    }
+}
