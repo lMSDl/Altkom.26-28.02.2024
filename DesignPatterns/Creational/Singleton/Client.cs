59b644317fc01af364e32823ceacfb5a34b08e64
Paweł Łęczycki <pawel@msd.info.pl>
2024-02-27 10:08:16 +0100

    Singleton I

---
+++ DesignPatterns/Creational/Singleton/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._26_28._02._2024.DesignPatterns.Creational.Singleton
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var service1 = new Context().GetSettings("1");
+            Console.WriteLine($"Service: {service1}");
+
+            var service2 = new Context().GetSettings("2");
+            Console.WriteLine($"Service: {service2}");
+
+            new Context().SetSettings("2", "X");
+            service2 = new Context().GetSettings("2");
+            Console.WriteLine($"Service: {service2}");
+        }
+
+    }
+}
