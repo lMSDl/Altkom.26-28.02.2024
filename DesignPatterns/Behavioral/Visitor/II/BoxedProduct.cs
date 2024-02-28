0b4b09ecd62edd0762de2ed9ae8db9d092709073
Paweł Łęczycki <pawel@msd.info.pl>
2024-02-28 15:37:06 +0100

    Visitor III

---
+++ DesignPatterns/Behavioral/Visitor/II/BoxedProduct.cs
@@ -0,0 +1,15 @@
+﻿namespace Altkom._26_28._02._2024.DesignPatterns.Behavioral.Visitor.II
+{
+    internal class BoxedProduct : Product
+    {
+        public override void PrintType()
+        {
+            Console.WriteLine("BoxedProduct");
+        }
+
+        public override void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/Visitor/II/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Altkom._26_28._02._2024.DesignPatterns.Behavioral.Visitor.II
+{
+    internal class Product : IElement
+    {
+        public virtual void PrintType()
+        {
+            Console.WriteLine("Product");
+        }
+
+        public virtual void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
