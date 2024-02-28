d5f5c7dfba7a0759bb114ddd90993ecb7b9f697e
Paweł Łęczycki <pawel@msd.info.pl>
2024-02-28 15:19:43 +0100

    Visitor I

---
+++ DesignPatterns/Behavioral/Visitor/I/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._26_28._02._2024.DesignPatterns.Behavioral.Visitor.I
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var collection = new IElement[]
+{
+                new PlainText() {Text = "Plain"},
+                new PlainText() { Text = "\n" },
+                new BoldText() {Text = "Bold"},
+                new PlainText() { Text = "\n" },
+                new Hyperlink() {Text = "Hyperlink",  Link=@"https:\\some.url.pl"}
+};
+
+
+            var builder = new StringBuilder();
+
+            foreach (var item in collection)
+            {
+                builder.Append(item.ToHtml());
+            }
+            Console.WriteLine( builder.ToString());
+        }
+    }
+}
