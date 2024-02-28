0eca4dbf37ef0863819139c25615874dc844e097
Paweł Łęczycki <pawel@msd.info.pl>
2024-02-28 10:04:13 +0100

    NullObject

---
+++ DesignPatterns/Behavioral/NullObject/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._26_28._02._2024.DesignPatterns.Behavioral.NullObject
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            //var objects = new AbstractObject?[] { new RealObject(), null };
+            var objects = new AbstractObject[] { new RealObject(), new NullObject() };
+            var random = new Random();
+
+            while (true)
+            {
+                var value = random.Next(0, objects.Length);
+                Console.ReadLine();
+
+                /*if(objects[value] != null)
+                    objects[value].Method1();
+
+                objects[value]?.Method2();
+
+                if (objects[value] is not null)
+                    objects[value].Method3();*/
+
+                objects[value].Method1();
+                objects[value].Method2();
+                objects[value].Method3();
+            }
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/NullObject/NullObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._26_28._02._2024.DesignPatterns.Behavioral.NullObject
+{
+    internal class NullObject : AbstractObject
+    {
+        public new void Method1()
+        {
+
+        }
+
+        public override void Method2()
+        {
+        }
+
+        public override void Method3()
+        {
+        }
+    }
+}
