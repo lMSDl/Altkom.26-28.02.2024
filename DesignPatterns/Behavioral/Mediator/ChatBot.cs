f821ccc7ece4b22d0d32afdc5cd4e30c77188bfb
Paweł Łęczycki <pawel@msd.info.pl>
2024-02-28 11:36:26 +0100

    Mediator

---
+++ DesignPatterns/Behavioral/Mediator/ChatBot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom._26_28._02._2024.DesignPatterns.Behavioral.Mediator
+{
+    internal class ChatBot : ChatMemeber
+    {
+        public override void Receive(string from, string message, bool isPrivate)
+        {
+            if (isPrivate)
+                Mediator.Send(Nick, from, message);
+            else
+                Mediator.Send(Nick, message);
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/Mediator/ChatMemeber.cs
@@ -0,0 +1,20 @@
+﻿namespace Altkom._26_28._02._2024.DesignPatterns.Behavioral.Mediator
+{
+    internal abstract class ChatMemeber
+    {
+        private IMediator mediator;
+        public string Nick { get; set; }
+        public IMediator Mediator
+        {
+            get => mediator;
+            set
+            {
+                mediator?.Quit(this);
+                mediator = value;
+                mediator?.Join(this);
+            }
+        }
+
+        public abstract void Receive(string from, string message, bool isPrivate);
+    }
+}
